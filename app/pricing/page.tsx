66f34de4bbd5c3a1de3a7407829e9c185851185b
AdhamMohamedSaleh <adham.work24@gmail.com>
2025-10-07 03:16:14 +0300

    01

---
+++ app/pricing/page.tsx
@@ -0,0 +1,15 @@
+import Plans from "@/components/layout/plans";
+
+export const metadata = {
+  title: "Pricing",
+}
+
+function Page() {
+  return (
+    <div>
+      <Plans />
+    </div>
+  );
+};
+
+export default Page;
